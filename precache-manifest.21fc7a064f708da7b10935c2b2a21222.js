2861084446c28ed85c9c8563f86a5ee7db41c0b3
Witanday <witanday.cd@gmail.com>
2020-03-26 00:36:52 -0400

    Updates

---
+++ precache-manifest.21fc7a064f708da7b10935c2b2a21222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25397242020d4f067bd97f35c380a13",
+    "url": "/Witanday/cardsFlyers/index.html"
+  },
+  {
+    "revision": "c4f7de8f3d4259c80e8e",
+    "url": "/Witanday/cardsFlyers/static/css/main.869dc402.chunk.css"
+  },
+  {
+    "revision": "85633a0d35f0fccdb548",
+    "url": "/Witanday/cardsFlyers/static/js/2.cc407b54.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Witanday/cardsFlyers/static/js/2.cc407b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f7de8f3d4259c80e8e",
+    "url": "/Witanday/cardsFlyers/static/js/main.dafc5091.chunk.js"
+  },
+  {
+    "revision": "968f092e105bab2fe92a",
+    "url": "/Witanday/cardsFlyers/static/js/runtime-main.71514d90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Witanday/cardsFlyers/static/media/logo.5d5d9eef.svg"
+  }
+]);
